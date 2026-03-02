a3586451daecad8b5deac3da270b5ed7135ca6e9
Udayvscode <uk009uk27@gmail.com>
2026-03-02 23:56:22 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,4 +15,13 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+// Routes import
+import userRouter from "./routes/user.routes.js"
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:5000/api/v1/users/register
+
 export { app }
--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect
         (`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`/n MongoDB connected !! DB HOST: $
+        console.log(`\n MongoDB connected !! DB HOST: $
             {connectionInstance.connection.host}`)
     } catch (error) {
         console.error("MONGODB connection FAILED:", error);
